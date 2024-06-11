a329a7e7dbc068abb7ef26c80da285d4cc214aec
Michael Galo <michael@michaelthinks.co>
2024-06-11 14:22:11 -0500

    pets module draft completed

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// import get functions from database.js
+
+// declare variables to target sections of the HTML in the DOM inside of main html container
+
+// declare variable for applicationHTML as an empty string
+
+// create a function for render that will render the applicationHTML to the DOM
+
+// use the .innerHTML method to render the applicationHTML to the DOM with the declared variables for the sections of the HTML in the DOM
---
+++ scripts/pets.js
@@ -0,0 +1,35 @@
+// import the getPets function from the database.js file
+import { getPets } from "./database.js";
+
+// declared a pet variable and set it equal to the getPets function
+const pets = getPets();
+
+// create an event listener for the click event on the button with the data-type of pets
+document.addEventListener("click", (clickEvent) => {
+  if (clickEvent.target.id === "pets") {
+    const currentPet = clickEvent.target;
+    window.alert(`This pet's name is ${currentPet}`);
+  }
+});
+
+// create a function to render the pets to the DOM and returning an HTML template string
+export const Pets = () => {
+  // declare a variable set to an empty string named petHTML
+  let petHTML = "";
+
+  // use a for...of loop to iterate over the pets array and return the pet object
+  for (const pet of pets) {
+    // add each pet object's name to the petHTML string while adding the state of data-type="pet" and the pet object's id and the pet's name
+    petHTML += `
+<li class="pet" 
+data-type="pet"
+data-ownerId=${pet.ownerId}  
+id=${pet.id}>
+${pet.name}
+</li>
+`;
+  }
+
+  // return the petHTML string
+  return petHTML;
+};
